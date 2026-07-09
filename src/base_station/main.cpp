cb16717516ad737ef3b71ba9b4b7426fc35b5ccc
milanbhutan <milanbhutan@gmail.com>
2026-07-08 20:34:29 -0700

    Added two RTOS tasks for reading command and telemetry queues

--- src/base_station/main.cpp
+++ src/base_station/main.cpp
@@ -1,14 +1,48 @@
 #include <Arduino.h>
 #include <esp_now.h>
-#include <Wifi.h>
+#include <WiFi.h>
 #include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 
-int myFunction(int, int);
+struct telemetry{};
+
+
+struct command{};
+
+
+
+void Command_Task(void *pCom_params);
+void Telemetry_Task(void *pTelem_params);
+
+TaskHandle_t Command_Task_ID=nullptr;
+TaskHandle_t Telemetry_Task_ID=nullptr;
 
 void setup() {
+  Serial.begin(9600);
+  
+  xTaskCreate(
+    Command_Task, 
+    "Command Task",
+    4096,
+    nullptr,
+    3,
+    &Command_Task_ID
+  );
 
-  int result = myFunction(2, 3);
+  xTaskCreate(
+    Telemetry_Task,
+    "Telemetry Task",
+    4096,
+    nullptr,
+    4,
+    &Telemetry_Task_ID
+  );
+
+
+
+
 }
 
 
@@ -17,9 +51,18 @@ void loop() {
 
 }
 
-
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void Command_Task(void *pCom_Params){
+  for(;;){
+    Serial.print("Command Task");
+    vTaskDelay(1000);
+  }
 }
+
+void Telemetry_Task(void *pTelem_Params){
+  for(;;){
+    Serial.print("Telemetry Task");
+    vTaskDelay(1000);
+
+  }
+}
+
